76a181a161e9714fe93fc2a783247846e3fc2750
Josiah Yeow <jsiahyeow@gmail.com>
2020-08-02 23:40:30 +1000

    add socket.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import socketIOClient from 'socket.io-client'
+
+const ENDPOINT = 'http://localhost:5000'
 
 function App() {
-  return <div>Emojicon</div>
+  const [response, setResponse] = useState('')
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT)
+    socket.on('fromAPI', (data: any) => {
+      setResponse(data)
+    })
+  }, [])
+  return <div>{response}</div>
 }
 
 export default App
