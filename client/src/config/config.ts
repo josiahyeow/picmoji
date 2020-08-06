371dcf4d892ced1a5a78cc5ad4358a15b6f7665e
Josiah Yeow <jsiahyeow@gmail.com>
2020-08-06 23:27:58 +1000

    add server url for deployed app

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,1 +1,4 @@
-export const SERVER_URL = 'http://localhost:5000'
+export const SERVER_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://emoji-game-0.herokuapp.com/'
