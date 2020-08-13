356f280f8dd9be4cd5d8d021eb4e3525d0d05f37
Josiah Yeow <jsiahyeow@gmail.com>
2020-08-13 22:43:05 +1000

    fix file paths

--- server/rooms/emojis.js
+++ server/rooms/emojis.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
+const path = require("path");
 
-let words = JSON.parse(fs.readFileSync("emojiSets/words.json"));
-let tv = JSON.parse(fs.readFileSync("emojiSets/tv.json"));
-let movies = JSON.parse(fs.readFileSync("emojiSets/movies.json"));
+const WORDS_JSON = path.join(__dirname, "../emojiSets/words.json");
+const TV_JSON = path.join(__dirname, "../emojiSets/tv.json");
+const MOVIES_JSON = path.join(__dirname, "../emojiSets/movies.json");
+
+let words = JSON.parse(fs.readFileSync(WORDS_JSON));
+let tv = JSON.parse(fs.readFileSync(TV_JSON));
+let movies = JSON.parse(fs.readFileSync(MOVIES_JSON));
 
 const emojiSets = {
   words,
