bb9c8bb0e521a9308b766bcb4d353688a6e4af52
Josiah Yeow <jsiahyeow@gmail.com>
2020-08-28 20:17:22 +1000

    add new datasets

--- server/data/emoji-sets/index.js
+++ server/data/emoji-sets/index.js
@@ -1,24 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const WORDS_JSON = path.join(__dirname, "./words.json");
-const TV_JSON = path.join(__dirname, "./tv.json");
+const ANIME_JSON = path.join(__dirname, "./anime.json");
+const BRANDS_JSON = path.join(__dirname, "./brands.json");
+const KOREABOO_JSON = path.join(__dirname, "./koreaboo.json");
 const MOVIES_JSON = path.join(__dirname, "./movies.json");
 const PLACES_JSON = path.join(__dirname, "./places.json");
-const ANIME_JSON = path.join(__dirname, "./anime.json");
+const TV_JSON = path.join(__dirname, "./tv.json");
+const WORDS_JSON = path.join(__dirname, "./words.json");
 
-let words = JSON.parse(fs.readFileSync(WORDS_JSON));
-let tv = JSON.parse(fs.readFileSync(TV_JSON));
+let anime = JSON.parse(fs.readFileSync(ANIME_JSON));
+let brands = JSON.parse(fs.readFileSync(BRANDS_JSON));
+let koreaboo = JSON.parse(fs.readFileSync(KOREABOO_JSON));
 let movies = JSON.parse(fs.readFileSync(MOVIES_JSON));
 let places = JSON.parse(fs.readFileSync(PLACES_JSON));
-let anime = JSON.parse(fs.readFileSync(ANIME_JSON));
+let tv = JSON.parse(fs.readFileSync(TV_JSON));
+let words = JSON.parse(fs.readFileSync(WORDS_JSON));
 
 const emojiSets = {
-  words,
-  tv,
+  anime,
+  brands,
+  koreaboo,
   movies,
   places,
-  anime,
+  tv,
+  words,
 };
 
 module.exports = { emojiSets };
