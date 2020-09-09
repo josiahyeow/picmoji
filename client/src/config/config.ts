5b510316f7d0f345406e88e1c9b3efdd1ed7c7c4
Josiah Yeow <josiahyeow.dev@gmail.com>
2020-09-09 22:46:18 +1000

    Revert "update app urls"
    This reverts commit 4134c1ee44c9fee96e67d6560d5fa94c65e57a79.

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export const config =
   process.env.NODE_ENV === 'development'
     ? {
@@ -10,3 +11,17 @@ export const config =
       }
 
 export default config
+=======
+export const config =
+  process.env.NODE_ENV === 'development'
+    ? {
+        SERVER_URL: 'http://localhost:5000',
+        CLIENT_URL: 'http://localhost:3000',
+      }
+    : {
+        SERVER_URL: 'https://picmoji.herokuapp.com',
+        CLIENT_URL: 'https://picmoji.herokuapp.com',
+      }
+
+export default config
+>>>>>>> parent of 4134c1e... update app urls
