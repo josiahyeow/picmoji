6dbbe4d7b61c93184e328fdcc9f9a9d6a9bb6c20
Josiah Yeow <josiahyeow.dev@gmail.com>
2020-10-18 22:20:39 +1100

    add Songs category

--- server/data/emoji-set.js
+++ server/data/emoji-set.js
@@ -22,10 +22,11 @@ async function fetchEmojis() {
       general: 1100919438,
       movies: 1448404852,
       tv: 1066868128,
+      songs: 109594337,
       places: 1066745410,
+      brands: 1563151545,
       anime: 554060221,
       koreaboo: 556464515,
-      brands: 1563151545,
     };
 
     const general = parseRows(
@@ -33,31 +34,34 @@ async function fetchEmojis() {
     );
     const movies = parseRows(await doc.sheetsById[SHEET_IDS.movies].getRows());
     const tv = parseRows(await doc.sheetsById[SHEET_IDS.tv].getRows());
+    const songs = parseRows(await doc.sheetsById[SHEET_IDS.songs].getRows());
     const places = parseRows(await doc.sheetsById[SHEET_IDS.places].getRows());
+    const brands = parseRows(await doc.sheetsById[SHEET_IDS.brands].getRows());
     const anime = parseRows(await doc.sheetsById[SHEET_IDS.anime].getRows());
     const koreaboo = parseRows(
       await doc.sheetsById[SHEET_IDS.koreaboo].getRows()
     );
-    const brands = parseRows(await doc.sheetsById[SHEET_IDS.brands].getRows());
 
     const emojiSets = {
-      anime,
-      brands,
-      koreaboo,
-      movies,
-      places,
-      tv,
+      songs,
       general,
+      movies,
+      tv,
+      places,
+      brands,
+      anime,
+      koreaboo,
     };
 
     console.log(
       `Fetched ${
         movies.length +
         tv.length +
+        songs.length +
         places.length +
+        brands.length +
         anime.length +
-        koreaboo.length +
-        brands.length
+        koreaboo.length
       } emoji sets.`
     );
 
--- server/data/rooms.js
+++ server/data/rooms.js
@@ -8,10 +8,11 @@ const DEFAULT_SELECTED_CATEGORIES = {
   general: { name: "General", icon: "💬", include: true },
   movies: { name: "Movies", icon: "🍿", include: false },
   tv: { name: "TV Shows", icon: "📺", include: false },
+  songs: { name: "Songs", icon: "🎵", include: false },
   places: { name: "Places", icon: "🌏", include: false },
+  brands: { name: "Brands", icon: "🛍", include: false },
   anime: { name: "Anime", icon: "🇯🇵", include: false },
   koreaboo: { name: "Koreaboo", icon: "🇰🇷", include: false },
-  brands: { name: "Brands", icon: "🛍", include: false },
 };
 
 function setEmojis(fetchedEmojis) {
